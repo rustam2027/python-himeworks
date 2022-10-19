c66b200505daa6fde340913fe6ba599593fc7aeb
Rustam Salimov <rustam2027@yandex.ru>
2022-10-19 14:06:51 +0700

    Completed 7th task

--- task_6-7.py
+++ task_6-7.py
@@ -1,14 +1,18 @@
 def flatten(arr: list, depth=-2):
+    if not isinstance(arr, list):
+        return arr
+    
     if depth == -1:
-        return [arr]
+        return arr
     else:
-        if isinstance(arr, list):
-            return_arr = []
-            for i in arr:
+        return_arr = []
+        for i in arr:
+            if isinstance(i, list):
                 return_arr = return_arr + flatten(i, depth - 1)
-            return return_arr
-        else:
-            return [arr]
+            else:
+                return_arr.append(i)
+        return return_arr
 
 
 print(flatten([1, 2, [4, 5], [6, [7]], 8], depth=2))
+print(flatten(13))
