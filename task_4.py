2324d22e9dc241fa3702e6e9236ca91a1229b7ee
Rustam Salimov <rustam2027@yandex.ru>
2022-10-19 14:19:00 +0700

    Completed 4th task

--- task_4.py
+++ task_4.py
@@ -10,13 +10,16 @@ def reverse_dict(dict):
 
     return_dict = {}
     items = dict.items()
+    is_more_than_two_values = {}
     for key, element in items:
         if element in return_dict:
-            if isinstance(return_dict[element], tuple):
+            if is_more_than_two_values[element]:
                 return_dict[element] = (*return_dict[element], key)
             else:
                 return_dict[element] = (return_dict[element], key)
+            is_more_than_two_values[element] = True
         else:
+            is_more_than_two_values[element] = False
             return_dict[element] = key
     return return_dict
 
@@ -28,3 +31,4 @@ doctest.testmod()
 
 print(reverse_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}))
 print(reverse_dict({"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832, "Sidorov": 97832}))
+print(reverse_dict({(1, 2): 97832, "Petrov": 55521, (2, 3): 97832, (4, 5): 97832}))
