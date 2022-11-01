fbe8d13efa1e59689009c02b2688c65716c4842e
rustam2027 <109891738+rustam2027@users.noreply.github.com>
2022-11-01 16:01:59 +0700

    Simplify 9th task

--- task_9.py
+++ task_9.py
@@ -1,15 +1,17 @@
 def format_table(name_column, name_line, data):
 
-    # find some const
+    NAME = "Benchmark"
+
+    # find some lens
     lines_len = []
     lines_len.append(max(len(max(name_column, key=lambda x: len(x))),
-                         len("Benchmark")) + 4)
+                         len(NAME)) + 4)
     for i in range(len(name_line)):
         lines_len.append(max(len(name_line[i]),
                              len(max(data, key=lambda x: len(str(x[i]))))) + 4)
 
     # printing
-    print(f"|{'benchmark'.center(lines_len[0])}|", end="")
+    print(f"|{NAME.center(lines_len[0])}|", end="")
     for i in range(len(name_line)):
         print(f"{name_line[i].center(lines_len[i + 1])}|", end="")
 
@@ -26,17 +28,17 @@ def format_table(name_column, name_line, data):
         print()
 
 
-format_table(["best case", "worst case"],
-             ["quick sort", "merge sort", "bubble sort"],
-             [[1.23, 1.56, 2.0], [3.3, 2.9, 3.9]])
-print()
+# format_table(["best case", "worst case"],
+#              ["quick sort", "merge sort", "bubble sort"],
+#              [[1.23, 1.56, 2.0], [3.3, 2.9, 3.9]])
+# print()
 
-format_table(["best case", "worst case"],
-             ["quick sort", "merge sort"],
-             [[1.23, 1.56], [3.3, 2.9]])
-print()
+# format_table(["best case", "worst case"],
+#              ["quick sort", "merge sort"],
+#              [[1.23, 1.56], [3.3, 2.9]])
+# print()
 
-format_table(["best case", "worst case", "middle case"],
-             ["quick sort", "merge sort", "bubble sort", "new sort"],
-             [[1.23, 1.56, 2.0, 3.0], [3.3, 2.9, 3.9, 6.0],
-              [1.0, 2.0, 3.0, 4.0]])
+# format_table(["best case", "worst case", "middle case"],
+#              ["quick sort", "merge sort", "bubble sort", "new sort"],
+#              [[1.23, 1.56, 2.0, 3.0], [3.3, 2.9, 3.9, 6.0],
+#               [1.0, 2.0, 3.0, 4.0]])
